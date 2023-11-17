f1b97451204fc5c047313ad592103419ad1f99db
abohatyrov <bohatyrovandrey@gmail.com>
2023-11-17 14:51:02 +0200

    Changes

--- main.tf
+++ main.tf
@@ -72,6 +72,8 @@ resource "aws_instance" "wordpress" {
 
   iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
 
+  private_ip = "172.31.42.242"  
+
   root_block_device {
     volume_size = 20
   }
