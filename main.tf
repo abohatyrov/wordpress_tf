36a67638403003727efdf7bfbf58b79687c3ae6d
abohatyrov <bohatyrovandrey@gmail.com>
2023-11-27 13:18:03 +0200

    Update Terraform

--- main.tf
+++ main.tf
@@ -65,6 +65,7 @@ module "cadvisor_container" {
   volumes = [
     { host_path = "/var/run", container_path = "/var/run", read_only = false },
     { host_path = "/sys", container_path = "/sys", read_only = true },
-    { host_path = "/", container_path = "/rootfs", read_only = true }
+    { host_path = "/", container_path = "/rootfs", read_only = true },
+    { host_path = "/var/lib/docker/", container_path = "/var/lib/docker", read_only = true }
   ]
 }
